84d48bd3629dba68cec9b850801582347d918fa5
Shubham Singh <mine.shubhamsingh@gmail.com>
2024-11-17 14:23:00 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.mode === "development"
+    import.meta.env.MODE === "development"
       ? "http://localhost:8080/api/v1"
       : "/api/v1",
   withCredentials: true, // send cookies with requests
